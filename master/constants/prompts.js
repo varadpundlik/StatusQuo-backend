cac95d9271fd5fbf3bb326fd1d69f168c21382db
varadpundlik <varadpundlik@gmail.com>
2024-04-02 14:57:38 +0530

    fix: Status Prompts and response

--- master/constants/prompts.js
+++ master/constants/prompts.js
@@ -1,4 +1,4 @@
 module.exports = {
-    statusPrompt: "I am sharing you the current code of a project of github repository and a json of features based on that code give the status of eact feature and task in JSON format.",
+    statusPrompt: "You are a AI project tracker and I am sharing you the current code of a project of github repository and a json of features based on that code give the status, description of work done in each feature and percentage of work done of eact feature and task in JSON format.",
     chatPrompt: "I am sharing you the current code of a project of github repository and following query based on that code answer tho that query",
 };
